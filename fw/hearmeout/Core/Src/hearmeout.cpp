4acfdd6db0e02eade45ad656e18a24083c360951
nbaskins <nbaskins@umich.edu>
2025-10-22 01:38:26 -0400

    added comments to hearmeout.cpp

--- fw/hearmeout/Core/Src/hearmeout.cpp
+++ fw/hearmeout/Core/Src/hearmeout.cpp
@@ -5,26 +5,28 @@
 
 extern TIM_HandleTypeDef htim1;
 
-uint8_t current_sample = 0;
-float audio_samples[NUM_SAMPLES] = {1.0f, 0.5f, 0.0f, 0.5f, 1.0f, 0.5f, 0.0f, 0.5f};
-Transducer t;
+uint8_t current_sample = 0; // current sample index
+float audio_samples[NUM_SAMPLES] = {1.0f, 0.5f, 0.0f, 0.5f, 1.0f, 0.5f, 0.0f, 0.5f}; // NUM_SAMPLES different sample values
+Transducer t; // transducer object, gets instantiated in init
 
+// main loop
 void event_loop() {
 	while (true) {}
 }
 
+// create transducer object and call event_loop
 void init() {
 	t = Transducer(&htim1, TIM_CHANNEL_1, TIM_CHANNEL_2, NUM_SAMPLES);
 	event_loop();
 }
 
 
+// HAL C functions
 extern "C" {
 
 // this callback is hit when the transducer sampling timer ccr value is reached
 void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim) {
-	// check that the timer and
-    if(htim->Instance == TIM1 && htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2) {
+    if(htim == htim1) {
     	t.play_sound(audio_samples[current_sample]);
     	current_sample = (current_sample + 1) % NUM_SAMPLES;
     }
