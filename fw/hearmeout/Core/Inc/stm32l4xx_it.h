a024ef89103e6a5f453aeaf222e2bfd0554565d1
nbaskins <nbaskins@umich.edu>
2025-12-03 01:30:20 -0500

    minor change to ioc

--- fw/hearmeout/Core/Inc/stm32l4xx_it.h
+++ fw/hearmeout/Core/Inc/stm32l4xx_it.h
@@ -63,6 +63,7 @@ void ADC1_IRQHandler(void);
 void EXTI9_5_IRQHandler(void);
 void TIM3_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
+void TIM5_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
